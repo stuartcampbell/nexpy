53487df29045153b504dcd23afbb1ad192721354
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-07-21 12:27:56 -0500

    Adding nxfileremote.py

---
+++ src/pyro/nxfileremote.py
@@ -0,0 +1,32 @@
+
+"""
+NXFileRemote
+The wrapper class representing a remote NX file
+Contains a Pyro proxy
+"""
+
+import sys
+import Pyro4
+
+import nexpy.api.nexus as nx
+from nexpy.api.nexus import NXFile
+
+import numpy as np
+
+def message(msg):
+    print("pyro client: " + str(msg))
+
+class NXFileRemote(NXFile):
+
+    def __init__(self, uri, name):
+        # Get a Pyro proxy to the remote object
+        Pyro4.config.SERIALIZER = "pickle"
+        self._file = Pyro4.Proxy(uri)
+        b = self._file.initfile(name)
+        assert(b)
+
+    def __getitem__(self, key):
+        return self._file.__getitem__(key)
+
+    def __exit__(self, *args):
+        self._file.exit(0)
