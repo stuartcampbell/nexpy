6fdcc75045d09875623725b468457afa9170dcee
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-07-21 13:21:22 -0500

    Improve input

--- src/pyro/nxfr_test.py
+++ src/pyro/nxfr_test.py
@@ -7,16 +7,23 @@ from nxfileremote import NXFileRemote
 def message(msg):
     print("pyro test: " + str(msg))
 
-if len(sys.argv) != 3:
-    print "usage: client.py <URI> <FILENAME>"
+if len(sys.argv) == 1:
+    name = raw_input("Enter file name: ")
+    uri = raw_input("Enter URI: ")
+elif len(sys.argv) == 2:
+    name = sys.argv.pop()
+    uri = raw_input("Enter URI: ")
+elif len(sys.argv) == 3:
+    uri  = sys.argv.pop()
+    name = sys.argv.pop()
+else:
+    print "usage: client.py <NAME> <URI>?"
     exit(1)
 
-uri = sys.argv[1]
-name = sys.argv[2]
-message("opening remote file: " + name)
+message("opening remote file: " + name + " on URI: " + uri)
 
 with NXFileRemote(uri, name) as nxfr:
-    print("file: ") #  + str(nxfr._file))
+    # print("file: ") #  + str(nxfr._file))
     f = nxfr["/entry/data/v"]
     f[0,0,0]
     a = nxfr[("/entry/data/v",[0,0,0])]
